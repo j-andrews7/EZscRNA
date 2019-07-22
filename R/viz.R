329b434a7abe81c0b14b77522d4f828e0ec6903a
Jared Andrews <jared.andrews07@gmail.com>
2019-07-22 14:05:03 -0500

    Add VizVlnPlot to viz.R

--- R/viz.R
+++ R/viz.R
@@ -1,67 +1,3 @@
-#' Visualize an annotated marker list
-#'
-#' \code{VizAnnotatedMarkers} creates Seurat FeaturePlots for lists of 
-#' marker genes associated with a given annotation.
-#'
-#' Plots for each annotation set will be output to a PDF that will be
-#' dynamically sized and named based on the name and number of markers for the
-#' set.
-#'
-#' @param scrna Seurat object.
-#' @param marker.file Tab-delimited file with the first two columns named "Set"
-#'   and "Marker". The "Set" column should contain a cell or process-type (e.g. 
-#'   Tcell, Bcell, Exhaustion markers, etc.) while the "Marker" column contains
-#'   the gene symbol associated with it. Many rows can be added to visualize 
-#'   many "Markers" for each "Set". 
-#' @param outdir Path to output directory.
-#' @param pt.size Use to adjust point size for plotting. 
-#' 
-#' @export
-#'
-VizAnnotatedMarkers <- function(scrna, marker.file, outdir, pt.size = NULL) {
-	# Read and filter the gene lists.
-	message("Parsing and filtering gene lists.")
-	glists.raw <- read.table(marker.file, sep = "\t", row.names = NULL, 
-		header = TRUE, as.is = TRUE)
-  glists <- glists.raw[which(glists.raw$Marker %in% rownames(scrna)), ] 
-
-  # Plot individual genes in various classes.
-  message("Plotting gene sets.")
-  for (i in unique(glists$Set)) {
-    gplotlist <- list()
-    # Remove problematic characters from cell classes.
-    j <- gsub(" ", "_", i)
-    j <- gsub("/", "_", j)
-    message("Plotting ", j)
-    genesToPlot <- glists$Marker[which(glists$Set == i)]
-    ng <- length(genesToPlot) 
-    out.tsne <- sprintf("%s/TSNE.%s.pdf", outdir, j)
-    out.umap <- sprintf("%s/UMAP.%s.pdf", outdir, j)
-
-    # Dynamic figure sizing.
-    h = 0
-    w = 0
-    if (ng > 1) {
-      w = 10 # two columns
-      h = 5 * (floor(ng / 2) + ng %% 2) # number of rows
-    } else {
-      w = 5 
-      h = 5 
-    }
-    pdf(out.tsne, useDingbats = FALSE, height = h, width = w)
-    fp <- FeaturePlot(object = scrna, features = genesToPlot, 
-    	cols = c("gray","red"), ncol = 2, reduction = "tsne", pt.size = pt.size)
-    print(fp)
-    dev.off()
-
-    pdf(out.umap, useDingbats = FALSE, height = h, width = w)
-    fp <- FeaturePlot(object = scrna, features = genesToPlot, 
-    	cols = c("gray","red"), ncol = 2, reduction = "umap", pt.size = pt.size)
-    print(fp)
-    dev.off()
-  }
-}
-
 #' Visualize by metadata variables
 #'
 #' \code{VizDimMetaData} creates Seurat DimPlots for the given meta.data columns
@@ -69,6 +5,7 @@ VizAnnotatedMarkers <- function(scrna, marker.file, outdir, pt.size = NULL) {
 #' saved in a PDF in the specified output directory.
 #'
 #' @param scrna Seurat object.
+#' @param vars Vector of meta.data column names to plot.
 #' @param outdir Path to output directory.
 #' @param pt.size Use to adjust point size for plotting. 
 #'
@@ -77,8 +14,42 @@ VizAnnotatedMarkers <- function(scrna, marker.file, outdir, pt.size = NULL) {
 #'
 #' @export
 #'
-VizDimMetaData <- function(scrna, vars, outdir, pt.size = NULL) {
+VizMetaData <- function(scrna, vars, outdir, pt.size = NULL) {
+	message("Plotting meta.data variables.")
 
+	for (i in vars) {
+		message("Plotting ", i)
+	  # Get all unique elements of variable.
+		vars_found <- sort(unique(scrna@meta.data[[as.character(i)]]))
+		  
+		# Color with rainbow colors.
+		cell_colors <- rainbow(length(vars_found), s = 0.6, v = 0.9)
+
+		w <- 10 + (2 * ceiling((length(vars_found) / 12)))
+
+		# Plot.
+		pdf(sprintf("%s/UMAP.TSNE.%s.pdf", outdir, i), width = w, height = 5, 
+			useDingbats=FALSE)
+		p1 <- DimPlot(object = scrna, group.by = as.character(i), 
+			cols = cell_colors, reduction = "tsne", pt.size = pt.size) + NoLegend()
+		p2 <- DimPlot(object = scrna, group.by = as.character(i), 
+			cols = cell_colors, reduction = "umap", pt.size = pt.size) + 
+			theme(legend.text = element_text(size = 7)) +
+			labs(color = as.character(i))
+
+		# Finicky garbage to get legends to not overlay plots.
+		p2a <- p2 + theme(legend.position = "none")
+		legend <- get_legend(p2)
+		plots <- align_plots(p1, p2a, align = 'h', axis = '0')
+		leg.w <- ceiling((length(vars_found) / 12)) * 0.4
+		c <- plot_grid(
+		  plots[[1]], plots[[2]], legend,
+		  rel_widths = c(1, 1, leg.w),
+		  nrow = 1
+		)
+		print(c)
+		dev.off()
+	}
 }
 
 
@@ -112,11 +83,11 @@ VizCellType <- function(scrna, outdir, pt.size = NULL) {
 	pdf(sprintf("%s/UMAP.TSNE.celltype.pdf", outdir), width = w, height = 5, 
 		useDingbats=FALSE)
 	p1 <- DimPlot(object = scrna, group.by = "lineage", cols = cell_colors, 
-		pt.size = 0.3, reduction = "tsne", pt.size = pt.size) + NoLegend()
+		reduction = "tsne", pt.size = pt.size) + NoLegend()
 	p2 <- DimPlot(object = scrna, group.by = "lineage", cols = cell_colors, 
-		pt.size = 0.3, reduction = "umap", pt.size = pt.size) + 
+		reduction = "umap", pt.size = pt.size) + 
 		theme(legend.text = element_text(size = 7)) +
-		labs(color='Cell Lineage')
+		labs(color = 'Cell Lineage')
 
 	# Finicky garbage to get legends to not overlay plots.
 	p2a <- p2 + theme(legend.position = "none")
@@ -125,7 +96,6 @@ VizCellType <- function(scrna, outdir, pt.size = NULL) {
 	leg.w <- ceiling((lineages / 12)) * 0.4
 	c <- plot_grid(
 	  plots[[1]], plots[[2]], legend,
-	  labels = c("A", "B"),
 	  rel_widths = c(1, 1, leg.w),
 	  nrow = 1
 	)
@@ -245,5 +215,115 @@ VizVDJDist <- function(scrna, outdir, g.by = NULL, o.by = NULL, n.clono.c = 10,
 			print(p)
 	}
 
+	dev.off()
+}
+
+#' Visualize an annotated marker list
+#'
+#' \code{VizAnnotatedMarkers} creates Seurat FeaturePlots for lists of 
+#' marker genes associated with a given annotation.
+#'
+#' Plots for each annotation set will be output to a PDF that will be
+#' dynamically sized and named based on the name and number of markers for the
+#' set.
+#'
+#' @param scrna Seurat object.
+#' @param marker.file Tab-delimited file with the first two columns named "Set"
+#'   and "Marker". The "Set" column should contain a cell or process-type (e.g.
+#'   Tcell, Bcell, Exhaustion markers, etc.) while the "Marker" column contains
+#'   the gene symbol associated with it. Many rows can be added to visualize 
+#'   many "Markers" for each "Set". 
+#' @param outdir Path to output directory.
+#' @param pt.size Use to adjust point size for plotting.
+#' @param vln Boolean indicating whether to create Seurat VlnPlots for each set.
+#'   Splits by cell idents.
+#' @param ridge Boolean indicating whether to create Seurat RidgePlots for each
+#'   set. Splits by cell idents.
+#' @param dot Boolean indicating whether to create Seurat DotPlots for each
+#'   set. Splits by cell idents.
+#' @param heatmap Boolean indicating whether to create a Seurat Heatmap for each
+#'   set. Splits by cell idents.
+#'
+#' @import Seurat
+#' 
+#' @export
+#'
+VizAnnotatedMarkers <- function(scrna, marker.file, outdir, pt.size = NULL,
+	vln = NULL, ridge = NULL, dot = NULL, heat = NULL) {
+	# Read and filter the gene lists.
+	message("Parsing and filtering gene lists.")
+	glists.raw <- read.table(marker.file, sep = "\t", row.names = NULL, 
+		header = TRUE, as.is = TRUE)
+  glists <- glists.raw[which(glists.raw$Marker %in% rownames(scrna)), ] 
+
+  # Plot individual genes in various classes.
+  message("Plotting gene sets.")
+  for (i in unique(glists$Set)) {
+    gplotlist <- list()
+    # Remove problematic characters from cell classes.
+    j <- gsub(" ", "_", i)
+    j <- gsub("/", "_", j)
+    message("Plotting ", j)
+    genesToPlot <- glists$Marker[which(glists$Set == i)]
+    ng <- length(genesToPlot) 
+    out.tsne <- sprintf("%s/TSNE.%s.pdf", outdir, j)
+    out.umap <- sprintf("%s/UMAP.%s.pdf", outdir, j)
+
+    # Dynamic figure sizing.
+    h = 0
+    w = 0
+    if (ng > 1) {
+      w = 10 # two columns
+      h = 5 * (floor(ng / 2) + ng %% 2) # number of rows
+    } else {
+      w = 5 
+      h = 5 
+    }
+    pdf(out.tsne, useDingbats = FALSE, height = h, width = w)
+    fp <- FeaturePlot(object = scrna, features = genesToPlot, 
+    	cols = c("gray","red"), ncol = 2, reduction = "tsne", pt.size = pt.size)
+    print(fp)
+    dev.off()
+
+    pdf(out.umap, useDingbats = FALSE, height = h, width = w)
+    fp <- FeaturePlot(object = scrna, features = genesToPlot, 
+    	cols = c("gray","red"), ncol = 2, reduction = "umap", pt.size = pt.size)
+    print(fp)
+    dev.off()
+
+
+  }
+}
+
+
+# Internal======================================================================
+
+#' Creates VlnPlots for gene sets
+#'
+#' Dynamically generates Seurat VlnPlots for all genes in a given gene set as a
+#' PDF.
+#'
+#' @param scrna Seurat object.
+#' @param outfile Path for output PDF.
+#' @param genes Vector of genes to plot.
+#'
+#' @importFrom Seurat VlnPlot
+#'
+#'
+VizVlnPlot <- function(scrna, outfile, genes) {
+	ng <- length(genes)
+	if (ng == 1) {
+	  w <- 0.3 * length(sort(unique(Idents(scrna))))
+	  h <- 4 * ceiling(length(genes) / 3)
+	} else if (ng == 2) {
+	  w <- 2 * (0.3 * length(sort(unique(Idents(scrna)))))
+	  h <- 4 * ceiling(length(genes) / 3)
+	} else {
+	  w <- 3 * (0.3 * length(sort(unique(Idents(scrna)))))
+	  h <- 4 * ceiling(length(genes) / 3)
+	}
+	pdf(outfile, useDingbats = FALSE, height = h, width = w)
+	p <- VlnPlot(scrna, features = genes) + NoLegend()
+	print(p)
 	dev.off()
 }
